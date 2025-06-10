8e88ce80c5b9470f224f67ce5f61f16468b23430
Sudipto <sudiptopatra025555@gmail.com>
2025-06-10 13:51:42 +0530

    initial commit

--- app/main.py
+++ app/main.py
@@ -1,5 +1,12 @@
 from fastapi import FastAPI
 from .routes import router
+from fastapi.middleware.cors import CORSMiddleware
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app = FastAPI()
 app.include_router(router)
